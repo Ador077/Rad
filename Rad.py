3e5ba98b8c69a1b6912da79d7674aab1c90c2cf1
Ado76 <119083581+Ador077@users.noreply.github.com>
2023-02-21 06:23:57 +0300

    Update Rad.py

--- Rad.py
+++ Rad.py
@@ -228,6 +228,7 @@ def x():
 		print('\033[1;32m[\033[1;32m✓] This massage is for my hatters')
 		print('[\033[1;32m✓]      The FIRE Is Back')
 		print('[\033[1;32m✓] Successfully Update Done 3:2.1')
+		print('SPEED BOOST \033[1;34m('\033[1;32mON/\033[38;5;196mOF\033[1;34m) AIRPLANE MODE')
 		print(50*'_')
 		
 		for guru in user:
@@ -259,6 +260,7 @@ def virus():
 		os.system('clear')
 		print(logo)
 		tl = str(len(user))
+		print('\033[1;32m
 		print('\033[1;32m[\033[1;32m✓] This massage is for my hatters')
 		print('[\033[1;32m✓]      The FIRE Is Back')
 		print('[\033[1;32m✓] Successfully Update Done 3:2.1')
